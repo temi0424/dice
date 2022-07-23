83abdc6342d026cb9e04679e67c6ef4169f4dc51
temi0424 <themieola@gmail.com>
2022-07-23 23:32:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var n = Math.random();
+n=Math.floor(n*6)+1;
+var randomNumber1 ="images/dice"+n+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomNumber1);
+
+
+
+var i = Math.random();
+i=Math.floor(i*6)+1;
+var randomNumber2 ="images/dice"+i+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomNumber2);
+
+
+if(randomNumber1<randomNumber2){
+document.querySelector("h1").textContent="player2 wins🚩"
+}
+else if (randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="🚩player1 wins"
+    }
+else {
+    document.querySelector("h1").textContent="🚩draw🚩"
+}
